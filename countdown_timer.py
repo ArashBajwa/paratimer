b23f4a416d28415fe06433f7d81a05c6f56c4adb
sakrosen <sakrosen@gmail.com>
2020-07-03 04:33:06 +0200

    Changed naming convention

--- countdown_timer.py
+++ countdown_timer.py
@@ -6,18 +6,24 @@ class CountdownTimer:
 		self.steppedBeyondZero = False
 		self.outputStringFormat = "{0}"
 
-
-	def startAt(self, currentTime):
+	def countdown_from(self, currentTime):
 		self.currentTime = currentTime
+		if currentTime < 0:
+			self.steppedBeyondZero = True
+			self.outputStringFormat = "-{0}"
+		else:
+			self.steppedBeyondZero = False
+			self.outputStringFormat = "{0}"
 
 	def step(self, amount=1):
 		self.currentTime -= amount
-		if self.currentTime < 0 and self.steppedBeyondZero == False:
+		if self.currentTime < 0:
 			self.steppedBeyondZero = True
 			self.outputStringFormat = "-{0}"
 		else:
 			self.steppedBeyondZero = False
 			self.outputStringFormat = "{0}"
 	
-	def getTimeStepped(self):
+	#returns string value of how much time passed H:M:S
+	def get_time_stepped(self):
 		return self.outputStringFormat.format(str(timedelta(seconds=abs(self.currentTime))))
